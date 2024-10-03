5f36e519852a29687783e719fc64b7350c645c5d
Mizrkat <r4dh1m1z3@gmail.com>
2024-10-03 20:36:05 +0700

    Mengunggah file Method

---
+++ Method.py
@@ -0,0 +1,14 @@
+class Percobaan:
+
+    def Menu_Makanan():
+        print("\n===Pilih menu makanan===")
+        print("1 = Nasi goreng (Rp15.000,00)")
+        print("2 = Mie goreng (Rp7.000,00)")
+        print("3 = Mie rebus (Rp10.000,00)")
+        print("4 = Nasi ayam (Rp20.000,00)\n")
+
+    def Metode_Pembayaran():
+        print("\nPilih Metode Pembayaran:")
+        print("1 = COD, tidak ada biaya tambahan")
+        print("2 = QRIS, terdapat biaya tambahan 1000")
+        print("3 = Transfer bank, terdapat biaya tambahan 2000\n")
