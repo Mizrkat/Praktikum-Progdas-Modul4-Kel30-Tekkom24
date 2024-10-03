9adc16e22e5e8f37329a3a948e225c1cec821fb7
Mizrkat <r4dh1m1z3@gmail.com>
2024-10-03 20:59:26 +0700

    Update komen Method.py

--- Method.py
+++ Method.py
@@ -1,6 +1,6 @@
 class Percobaan:
 
-    //Method menu makanan
+    #Method menu makanan
     def Menu_Makanan():
         print("\n===Pilih menu makanan===")
         print("1 = Nasi goreng (Rp15.000,00)")
@@ -8,7 +8,7 @@ class Percobaan:
         print("3 = Mie rebus (Rp10.000,00)")
         print("4 = Nasi ayam (Rp20.000,00)\n")
 
-    //Method cara pembayaran
+    #Method cara pembayaran
     def Metode_Pembayaran():
         print("\nPilih Metode Pembayaran:")
         print("1 = COD, tidak ada biaya tambahan")
